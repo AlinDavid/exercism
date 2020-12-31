9aee57a2524541c1a7d079dcdeca5ea37a2e5b02
David Alin <70276073+AlinTobirama@users.noreply.github.com>
2020-12-31 17:20:25 +0200

    Add files via upload

---
+++ yacht.py
@@ -0,0 +1,73 @@
+"""
+This exercise stub and the test suite contain several enumerated constants.
+
+Since Python 2 does not have the enum module, the idiomatic way to write
+enumerated constants has traditionally been a NAME assigned to an arbitrary,
+but unique value. An integer is traditionally used because it’s memory
+efficient.
+It is a common practice to export both constants and functions that work with
+those constants (ex. the constants in the os, subprocess and re modules).
+
+You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
+"""
+
+def YACHT(dice):
+    if dice.count(dice[0]) == 5:
+        return 50
+    else:
+        return 0
+
+def ONES(dice):
+    return 1 * dice.count(1)
+
+def TWOS(dice):
+    return 2 * dice.count(2)
+
+def THREES(dice):
+    return 3 * dice.count(3)
+
+def FOURS(dice):
+    return 4 * dice.count(4)
+
+def FIVES(dice):
+    return 5 * dice.count(5)
+
+def SIXES(dice):
+    return 6 * dice.count(6)
+
+def FULL_HOUSE(dice):
+    first_digit = dice.count(dice[0])
+
+    for digit in dice[1:]:
+        if digit != first_digit:
+            second_digit = dice.count(digit)
+            break
+
+    return sum(dice) if (first_digit == 2 and second_digit == 3) or (first_digit == 3 and second_digit == 2) else 0
+
+def FOUR_OF_A_KIND(dice):
+    dice.sort()
+    if dice.count(dice[2]) >= 4:
+        return 4 * dice[2]
+    else:
+        return 0
+
+def LITTLE_STRAIGHT(dice):
+    for i in range(1, 6):
+        if dice.count(i) != 1:
+            return 0
+
+    return 30
+
+def BIG_STRAIGHT(dice):
+    for i in range(2, 7):
+        if dice.count(i) != 1:
+            return 0
+
+    return 30
+
+def CHOICE(dice):
+    return sum(dice)
+
+def score(dice, category):
+    return category(dice)
