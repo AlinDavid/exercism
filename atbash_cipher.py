6d20107e61db3ac7529cbb8ec5edf98d8f169b46
David Alin <70276073+AlinTobirama@users.noreply.github.com>
2021-01-03 19:50:05 +0200

    Add files via upload

---
+++ atbash_cipher.py
@@ -0,0 +1,11 @@
+# def encode(plain_text):
+#     n = ord('z') + ord('a')
+#     return "".join([chr(n - ord(s)) for s in plain_text if s != " "])
+#
+#
+# def decode(ciphered_text):
+#     n = ord('z') + ord('a')
+#     return "".join([chr(n - ord(s)) for s in ciphered_text if s != " "])
+
+x = "xsz"
+print(x[1])
