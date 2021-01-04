f6c0aa14fe11d42e50d02cd93c7a502c0e80f44f
David Alin <70276073+AlinTobirama@users.noreply.github.com>
2021-01-04 21:33:03 +0200

    Add files via upload

---
+++ flatten_array.py
@@ -0,0 +1,8 @@
+def flatten(iterable):
+    output = []
+    for i in iterable:
+        if (isinstance(i,list)):
+            output = output + flatten(i)
+        else:
+            output = output + [i] if (i != None) else output
+    return output
