c6ce80496f4e3e936cb42d7a8f2cc981b1fc1762
David Alin <70276073+AlinTobirama@users.noreply.github.com>
2021-01-06 10:22:32 +0200

    Update atbash_cipher.py

--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,11 +1,8 @@
-# def encode(plain_text):
-#     n = ord('z') + ord('a')
-#     return "".join([chr(n - ord(s)) for s in plain_text if s != " "])
-#
-#
-# def decode(ciphered_text):
-#     n = ord('z') + ord('a')
-#     return "".join([chr(n - ord(s)) for s in ciphered_text if s != " "])
+def encode(plain_text):
+    n = ord('z') + ord('a')
+    return "".join([chr(n - ord(s)) for s in plain_text if s != " "])
 
-x = "xsz"
-print(x[1])
+
+def decode(ciphered_text):
+    n = ord('z') + ord('a')
+    return "".join([chr(n - ord(s)) for s in ciphered_text if s != " "])
