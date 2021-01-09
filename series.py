2fefccca8e64d9088b55b312f96562f500dcad88
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-09 21:27:11 +0200

    Update series.py

--- series.py
+++ series.py
@@ -1,12 +1,6 @@
 def slices(series, length):
+    
     if length > len(series):
         raise ValueError("Your substring is too large")
+        
     return [series[i : i + length] for i in range(0, len(series) - length + 1)]
-
-
-
-
-
-
-
-
