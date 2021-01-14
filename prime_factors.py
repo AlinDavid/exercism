8488be7d367634b61601f486b4dcf217b44d0a2b
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-14 09:57:06 +0200

    Add files via upload

---
+++ prime_factors.py
@@ -0,0 +1,18 @@
+def prime(number):
+    divisors = [i for i in range(2, number // 2) if number % i == 0]
+
+    if len(divisors) <= 2:
+        return True
+    else:
+        return False
+
+
+def factors(value):
+
+    multiple_list = []
+    for i in range(2, value):
+        while prime(i) is True and value % i == 0:
+            value /= i
+            multiple_list.append(i)
+
+    return multiple_list
