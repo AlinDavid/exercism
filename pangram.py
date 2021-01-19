56d39bf9df92f18086eb305abe7ce27ddc47cd52
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-19 14:21:28 +0200

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,13 +1,6 @@
 
-
 def is_pangram(sentence):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     for char in alphabet:
         if char not in sentence.lower():
             return False
-
-
-if is_pangram("Jock nymphs waqf drug vex blitz") is False:
-    print("This sentence is not a pangram")
-else:
-    print("This sentence is a pangram")
