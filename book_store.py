e0577b452559d86a7103688a548f634427e7dc16
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-21 19:28:31 +0200

    Update book_store.py

--- book_store.py
+++ book_store.py
@@ -5,19 +5,26 @@ def total(basket):
         return 0
 
     book_sets = [set()]
+    
     for book in basket:
+        
         allocated = False
+        
         for book_set in book_sets:
+            
             if book not in book_set:
                 book_set.add(book)
                 allocated = True
                 break
+                
         if not allocated:
             book_sets.append({book})
 
     sets_of_three = [s for s in book_sets if len(s) == 3]
     sets_of_five = [s for s in book_sets if len(s) == 5]
+    
     for set_five, set_three in zip(sets_of_five, sets_of_three):
+        
         book = (set_five - set_three).pop()
         set_five.remove(book)
         set_three.add(book)
@@ -26,6 +33,7 @@ def total(basket):
     discount = {1: 0, 2: 5, 3: 10, 4: 20, 5: 25}
 
     for book_set in book_sets:
+        
         set_total = len(book_set) * book_price
         set_total -= set_total * discount[len(book_set)] / 100
         total_sum += set_total
