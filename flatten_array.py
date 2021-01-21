32bc02819daca9a6cdbd8c2a2be333ff73ce1d76
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-21 19:30:21 +0200

    Update flatten_array.py

--- flatten_array.py
+++ flatten_array.py
@@ -1,8 +1,11 @@
 def flatten(iterable):
+    
     output = []
+    
     for i in iterable:
         if (isinstance(i,list)):
             output = output + flatten(i)
         else:
             output = output + [i] if (i != None) else output
+            
     return output
