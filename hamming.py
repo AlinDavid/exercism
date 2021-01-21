1f534d903ef9a9ab68fe5abc24beee9e90e201de
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-21 19:31:13 +0200

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,8 +1,7 @@
 
-
 def distance(strand_a, strand_b):
+    
     if len(strand_b) != len(strand_a):
         raise ValueError("Those 2 strings must be equal")
 
     return sum(a != b for a, b in zip(strand_a, strand_b))
-
